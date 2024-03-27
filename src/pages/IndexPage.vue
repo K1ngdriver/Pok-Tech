e3d8363bcb1e3ac12f252553d0726de1160f714b
Lucas Barbosa <lucas.oli.barb@gmail.com>
2024-03-26 21:40:42 -0300

    Protótipos do Professor aula 1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <button @click="count++">{{ count }}</button>
+    <input v-model="message" @keyup.esc="clearMessage" />
+    <button @click="clearMessage">Clear</button>
+    <h6 v-show="message.length">Tipying...</h6>
+    <h5 v-if="message.length">{{ message }}</h5>
+    <h6 v-else>No message entered</h6>
+    <hr />
+    <h6>Uppercase Message: {{ upperCaseMessage }}</h6>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+      message: "First vue application",
+      count: 0,
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    handleKeyup(e) {
+      //console.log(e);
+      if (e.keycode == 27) this.clearMessage();
+    },
+  },
+
+  computed: {
+    upperCaseMessage() {
+      console.log("upperCaseMessage fired");
+      return this.message.toUpperCase();
+    },
+  },
 });
 </script>
