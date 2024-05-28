0b84a0723b5b03ec80571335b6575c44567c7fd8
Lucas Barbosa <lucas.oli.barb@gmail.com>
2024-05-28 20:06:20 -0300

    commit de novas funções para o store

---
+++ src/store/tasks/state.js
@@ -0,0 +1,27 @@
+export default function () {
+  return {
+    tasks: [
+      {
+        id: "1",
+        name: "Ir ao shopping",
+        dueDate: "22/05/2024",
+        dueTime: "10:00",
+        completed: false,
+      },
+      {
+        id: "2",
+        name: "Comprar bananas",
+        dueDate: "27/05/2024",
+        dueTime: "15:00",
+        completed: false,
+      },
+      {
+        id: "3",
+        name: "Comprar roupas",
+        dueDate: "30/05/2024",
+        dueTime: "18:00",
+        completed: false,
+      },
+    ],
+  };
+}
