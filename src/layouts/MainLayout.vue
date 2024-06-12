06c1bea4ddfbcdb20d392d540152e5b4d5dd6360
Eduardo Garcia <eduardomgarcia335@gmail.com>
2024-06-11 21:40:17 -0300

    Configurações iniciais e listagem dos pokémons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,96 +11,101 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Teste App
-        </q-toolbar-title>
+        <q-toolbar-title class="title"> Teste App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header> Essential Links </q-item-label>
       </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
+      <div class="pokemon-list">
+        <div
+          v-for="pokemon in pokemons"
+          :key="pokemon.name"
+          class="pokemon-item"
+          @click="getPokemon(pokemon)"
+        >
+          <img :src="pokemon.image" :alt="pokemon.name" />
+          <div>{{ pokemon.name }}</div>
+        </div>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref, onMounted, onUnmounted } from "vue";
+import axios from "axios";
+import "./MainLayout.scss";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+const leftDrawerOpen = ref(false);
 
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
+
+function getPokemon(pokemon) {
+  console.log(pokemon);
+}
+
+const pokemons = ref([]);
+const offset = ref(0);
+const limit = 50;
+const loading = ref(false);
+const error = ref(null);
+
+async function fetchPokemons() {
+  loading.value = true;
+  error.value = null;
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?offset=${offset.value}&limit=${limit}`
+    );
+    const promises = response.data.results.map(async (pokemon) => {
+      const details = await axios.get(pokemon.url);
+
+      return {
+        name: pokemon.name,
+        image: details.data.sprites.front_default,
+      };
+    });
+    const newPokemons = await Promise.all(promises);
+
+    pokemons.value = [...pokemons.value, ...newPokemons];
+    offset.value += limit;
+  } catch (err) {
+    error.value = "Failed to load pokemons";
+  } finally {
+    loading.value = false;
+  }
+}
+
+function handleScroll() {
+  const bottomOfWindow =
+    window.innerHeight + window.scrollY >=
+    document.documentElement.scrollHeight - 10;
+  if (bottomOfWindow && !loading.value) {
+    fetchPokemons();
+  }
+}
+
+onMounted(() => {
+  fetchPokemons;
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
