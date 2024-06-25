70e1fb71c4ecef4a186fe53e70a8b863f31ab6c5
JoseVitorGarcia <josevitorguigarc@gmail.com>
2024-06-25 00:02:20 -0300

    added password style at login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,22 @@
 <template>
   <q-page class="flex flex-center">
     <ContainerComponent title="Login">
-      <div class="full-width q-pa-md">
-        <q-input v-model="objectData.email" label="E-mail" />
-        <q-input v-model="objectData.password" label="Senha" />
+      <div class="full-width q-pa-md" style="margin-bottom: 10px">
+        <q-input filled v-model="objectData.email" label="E-mail" />
+        <q-input
+          v-model="objectData.password"
+          filled
+          :type="isPwd ? 'password' : 'text'"
+          label="Senha"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="togglePwdVisibility"
+            />
+          </template>
+        </q-input>
         <q-btn @click="onSubmit">Entrar</q-btn>
       </div>
     </ContainerComponent>
@@ -12,6 +25,7 @@
 
 <script>
 import { mapActions } from "vuex";
+
 import ContainerComponent from "src/components/ContainerComponent.vue";
 
 export default {
@@ -25,11 +39,16 @@ export default {
         email: "",
         password: "",
       },
+      isPwd: true,
     };
   },
   methods: {
     ...mapActions("auth", ["login"]),
+    togglePwdVisibility() {
+      this.isPwd = !this.isPwd;
+    },
     async onSubmit() {
+      console.log("Logging in with:", this.objectData);
       await this.login(this.objectData);
       this.$router.push("/menu");
     },
